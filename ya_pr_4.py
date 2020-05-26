95715847b033f0faf5294935236f2227074dbfd5
kvadratpm <kvadratpm@gmail.com>
2020-05-26 18:56:41 +0300

    desicions 1-3 ya_pr_contest 'Algoritm' memory error

---
+++ ya_pr_4.py
@@ -0,0 +1,24 @@
+f = open('C:\Python\input.txt', 'r')
+file = f.readlines()
+f.close()
+m = int(file.pop(0))
+mass = [[int(n) for n in string.rstrip().split(' ')] for string in file]
+del file
+x = m // 2
+y = m // 2
+s = m - 1
+o = open('C:\Python\output.txt', 'w')
+for i in range(0, m ** 2):
+    o.write(f'{mass[y][x]}\n')
+    sum = x + y
+    if x <= y:
+        if sum <= s:
+            y -= 1
+        else:
+            x -= 1
+    elif x > y:
+        if sum < s:
+            x += 1
+        else:
+            y += 1
+o.close()
